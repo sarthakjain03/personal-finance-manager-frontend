df2ec1403d8d5bd34c326f851593a7b7dea76658
Sarthak <jainsarthak2003@gmail.com>
2025-04-27 16:56:45 +0530

    added layout and pages folders

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -7,12 +7,8 @@ import {
 import paths from "@/lib/constants/path";
 import useUserStore from "@/store/user-store";
 import { User } from "@/types/user";
-import PageNotFound from "@/components/app/page-not-found";
 
 const PageNotFoundView = lazy(() => import("@/components/app/page-not-found"));
-
-// TODO: import all page views as lazy components
-// TODO: create a User type and fill the createBrowserRouter array
 
 const createAppRouter = ({ user }: { user: User | null }) => {
   return createBrowserRouter([
