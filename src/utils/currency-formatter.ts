1eb5cd890636981cb60fc19a69c8f0e3fe0fae2b
Sarthak Jain <jainsarthak2003@gmail.com>
2025-07-10 23:40:22 +0530

    currency-formatter util update

--- src/utils/currency-formatter.ts
+++ src/utils/currency-formatter.ts
@@ -1,10 +1,26 @@
+import { User } from "@/types/user";
+
+const currencyLocales = {
+  USD: "en-US",
+  EUR: "de-DE",
+  GBP: "en-GB",
+  JPY: "ja-JP",
+  CNY: "zh-CN",
+  CAD: "en-CA",
+  AUD: "en-AU",
+  CHF: "de-CH",
+  INR: "en-IN",
+  KRW: "ko-KR",
+};
+
 const formatCurrency = (
   value: number | undefined | null,
-  currency: string = "INR"
+  user: User | null
 ) => {
+  const currency = user?.currencyFormat || "INR";
   const amount = value ? parseFloat(value.toFixed(2)) : 0;
 
-  const formatted = new Intl.NumberFormat("en-US", {
+  const formatted = new Intl.NumberFormat(currencyLocales[currency], {
     style: "currency",
     currency: currency,
   }).format(amount);
@@ -13,5 +29,3 @@ const formatCurrency = (
 };
 
 export default formatCurrency;
-
-// TODO: Add a currency selector in the navbar and in database of User as well.
